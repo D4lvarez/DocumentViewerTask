6c621418d8421fce2bc068842b2498bbe1adc310
Diego Alvarez <andresdaap2015@gmail.com>
2021-05-11 04:20:50 -0400

    Add support to display JPG/JPEG, PNG and PDF

---
+++ viewer.php
@@ -0,0 +1,48 @@
+<?php
+$pathFile = $_POST['path'];
+
+// Files must be inside project directory
+
+// Verify if file exists
+if (!is_readable($pathFile)) {
+	http_response_code(404);
+	echo "Cannot find that file";
+	die();
+}
+
+// Verify if extensions file to show
+$fileExtension = strtolower(pathInfo($pathFile, PATHINFO_EXTENSION));
+
+switch($fileExtension) {
+	case "pdf":
+		// Header Config for PDF Files
+		header("Content-type: application/pdf");
+		header("Content-Disposition: inline; filename=$pathFile");
+		header("Content-Length: ".filesize($pathFile));
+
+		// Send File to Browser
+		readfile($pathFile);
+		break;
+	case "png":
+		header("Content-type: image/png");
+		header("Content-Disposition: inline; filename=$pathFile");
+		header("Content-Length: ".filesize($pathFile));
+
+		// Send Image to Browser
+		echo file_get_contents($pathFile);
+		break;
+
+	case "jpeg":
+	case "jpg":
+		header("Content-type: image/jpeg");
+		header("Content-Disposition: inline; filename=$pathFile");
+		header("Content-Length: ".filesize($pathFile));
+
+		// Send Image to Browser
+		echo file_get_contents($pathFile);
+}
+
+
+
+
+?>
